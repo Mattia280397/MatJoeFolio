e32185471f7ba9f192e2ecc23f16f97accab044b
Mattia280397 <121868385+Mattia280397@users.noreply.github.com>
2023-04-14 14:49:07 +0200

    Fixed path of preventive and payment

--- src/app/personalarea/personalarea.module.ts
+++ src/app/personalarea/personalarea.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { PreventiveComponent } from '../preventive/preventive.component';
-import { PaymentComponent } from '../payment/payment.component';
+import { PreventiveComponent } from './preventive/preventive.component';
+import { PaymentComponent } from './payment/payment.component';
 import { PersonalareaComponent } from './personalarea.component';
 const routes: Routes = [
 
