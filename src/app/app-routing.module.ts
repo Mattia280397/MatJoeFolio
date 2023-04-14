f82fd482c5f2ffc222bed12a492721efdd36a2ca
Mattia280397 <121868385+Mattia280397@users.noreply.github.com>
2023-04-14 14:41:22 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { PreventiveComponent } from './preventive/preventive.component';
-import { PaymentComponent } from './payment/payment.component';
 import {LoginComponent} from "./login/login.component"
 const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'footer', component: FooterComponent },
   { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
   { path: 'aboutus', component: AboutUsComponent },
-  { path: 'navbar', component: NavbarComponent },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'preventive', component: PreventiveComponent },
-  { path: 'payment', component: PaymentComponent },
-  {path:'login', component:LoginComponent},
+  { path: "login", component: LoginComponent},
+  { path: 'personalarea', loadChildren: () => import('./personalarea/personalarea.module').then((m) => m.PersonalareaModule)},
 ];
 
 @NgModule({
